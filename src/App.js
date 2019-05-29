be9711d11505dff4bc4cb3a0cf949b0ab14860f9
newmann <newmannhu@qq.com>
2019-05-29 17:13:03 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {Button} from "./components";
+
+function App() {
+  return (
+    <div className="App">
+        <Button text={'hhhasdasdh'}></Button>
+    </div>
+  );
+}
+
+export default App;
